9b5d9d9334a89f4f734fd9c60998f941cfacc4e6
Dean Gilewicz <gilewiczd@gmail.com>
2014-11-21 00:05:19 -0500

    setting up routes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,33 @@
-console.log('The Iron Yard Rocks');
+(function () {
+
+  angular.module('AppontmentList', ['ngRoute'])
+
+  .constant ({
+    'appUrl': 'http://tiy-atl-fe-server.herokuapp.com/collections/appointments/'
+  })
+
+  .config( function ($routeProvider){
+
+    $routeProvider.when('/', {
+      templateUrl: 'templates/list-template.html',
+      controller: 'ListController'
+    });
+
+    $routeProvider.when('/add', {
+      templateUrl: 'templates/add-template.html',
+      controller: 'AddController'
+    });
+
+    $routeProvider.when('/single/:aid', {
+      templateUrl: 'templates/single-template.html',
+      controller: 'SingleController'
+    });
+
+    $routeProvider.otherwise({
+      templateUrl: 'templates/other-template.html',
+      controller: 'OtherController'
+    });
+
+  });
+
+}());
