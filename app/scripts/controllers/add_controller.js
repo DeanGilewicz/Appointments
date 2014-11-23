e4fe530ae545c205db9f416a909954b6b7a96c54
Dean Gilewicz <gilewiczd@gmail.com>
2014-11-23 18:41:12 -0500

    included appFactory function in add ctrl

--- app/scripts/controllers/add_controller.js
+++ app/scripts/controllers/add_controller.js
@@ -6,6 +6,7 @@
         function (appFactory, $scope, $rootScope, $location) {
 
           $scope.addAppt = function (appt) {
+            appFactory.addAppt(appt);
             $rootScope.$on('appt:added', function () {
               $location.path('/');
             });
